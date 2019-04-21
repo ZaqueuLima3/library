e7052da5a5873f74af86bfb6694548a629f7ff97
ZAQUEU LIMA DOS SANTOS <31195408+ZaqueuLima3@users.noreply.github.com>
2019-04-21 20:59:54 -0300

    Update home.php

--- src/views/home.php
+++ src/views/home.php
@@ -31,7 +31,7 @@
     <div class="col-md-3 col-12">
       <h3>Categorias</h3>
       <ul class="list-categories">
-        <a class="categories-items" href="<?php echo BASE_URL; ?>search">Todas cadecorias</a>
+        <a class="categories-items" href="<?php echo BASE_URL; ?>search">Todas categorias</a>
         <?php foreach($categories as $category): ?>
           <a class="categories-items" href="<?php echo BASE_URL; ?>search?c=<?php echo $category['id'] ?>"><li><?php echo $category['category_name']; ?></li></a>
         <?php endforeach; ?>
